e7d0c481d662bedf1098c6a26c1691045c6948b6
Miguelissimo <seux.1234@googlemail.com>
2014-02-15 13:46:04 +0100

    simple menu from tutorial

---
+++ bl-simple-menu.py
@@ -0,0 +1,20 @@
+import bpy
+
+class customMenu(bpy.types.Menu):
+    bl_label = "Custom Menu"
+    bl_idname = "View3D.custom_menu"
+    
+    def draw(self, context):
+        layout = self.layout
+        
+        layout.operator("mesh.primitive_cube_add")
+        
+def register():
+    bpy.utils.register_class(customMenu)
+    bpy.ops.wm.call_menu(name=customMenu.bl_idname)
+        
+def unreister():
+    bpy.utils.unregister_class(customMenu)
+        
+if __name__ == '__main__':
+    register()
